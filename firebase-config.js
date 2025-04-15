8920abde108697709c11207e37666ab3d01ee3de
Besys <branimir.berend@icloud.com>
2025-04-15 19:46:24 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyALZPAN6E9QsXbMvLtMe7rpxfOjUujfgWU",
+  authDomain: "besys-platforma.firebaseapp.com",
+  projectId: "besys-platforma",
+  storageBucket: "besys-platforma.firebasestorage.app",
+  messagingSenderId: "55197857239",
+  appId: "1:55197857239:web:469a737fc520ccab170792"
+};
